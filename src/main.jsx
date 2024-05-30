c00b8f076e90dbdf64d81b444da9eb9c55bccbc4
JrG-One <ojaswavarshney27@gmail.com>
2024-05-30 22:39:03 +0530

    Bug Solved

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
 import App from './App';
-import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
